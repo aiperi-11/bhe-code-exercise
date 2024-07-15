c78ad660b8113a04601cf08205c6b2d3f803a342
Aiperi Razaeva <aiperirazaeva@Aiperis-MBP.hsd1.il.comcast.net>
2024-07-15 17:03:50 -0500

    Nth prime logic added

--- javascript/Sieve/sieve.js
+++ javascript/Sieve/sieve.js
@@ -1,6 +1,25 @@
 class Sieve {
   NthPrime(n) {
-    throw new Error("unimplemented");
+
+    if (n <= 0) {
+      throw new Error("'n' must be bigger than 0");
+    }
+
+    // Get the upper limit for nth prime
+    const upperLimit = Math.ceil(n * Math.log(n) + n * Math.log(Math.log(n)));
+    const isPrime = new Array(upperLimit + 1).fill(true);
+    const primeNumbers = [];
+
+    for (let i = 2; i <= upperLimit; i++) {
+      if (isPrime[i]) {
+        primeNumbers.push(i);
+        for (let j = i * i; j <= upperLimit; j += i) {
+          isPrime[j] = false;
+        }
+      }
+    }
+
+    return primeNumbers[n - 1];
   }
 }
 
